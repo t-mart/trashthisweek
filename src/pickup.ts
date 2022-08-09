70b978d96cdf0f8949dfea0bd5c4ae51ee54a427
Tim Martin <tim@timmart.in>
2022-08-09 07:15:57 -0500

    Use "next <day> and "this <day>" depending on current date

--- src/pickup.ts
+++ src/pickup.ts
@@ -6,6 +6,7 @@ import {
   isSameDay,
   format,
   addDays,
+  isSameWeek,
 } from "date-fns";
 
 interface Pickup {
@@ -34,15 +35,38 @@ function getIsRecycling(date: Date) {
   return differenceInWeeks(KNOWN_RECYCLE_DATE, date) % 2 === 0;
 }
 
-// <Today, August 3rd>, is ...
-// <Tomorrow, August 3rd>, is ...
-// <Next Wednesday, August 3rd>,
+/**
+ * Describe toDate relative to fromDate in words.
+ *
+ * Note: this function is not generalized to all arbitrary dates. It only makes sense when
+ *
+ * - fromDate is earlier than toDate
+ * - fromDate and toDate are in the same week or consecutive weeks
+ *
+ * Examples:
+ * - "Today, August 3rd" if fromDate and toDate are the same day
+ * - "Tomorrow, August 3rd" if fromDate + 1 day is the same day
+ * - "This Wednesday, August 3rd" if fromDate is in the same week as toDate (week being the period
+ *   that starts 00:00 Sunday and ends 23:59 Saturday)
+ * - "Next Wednesay, August 3rd" in all other cases.
+ *
+ * The difference between "This Wednesday" and "Next Wednesday" follow an idiosyncrasy of English,
+ * where the meaning of "this" and "next" change depending on the instant of time in which they are
+ * used. See https://english.stackexchange.com/questions/3841/which-day-does-next-tuesday-refer-to
+ * for more discussion.
+ *
+ * @param fromDate the reference date from which to describe toDate
+ * @param toDate the date to describe
+ * @returns text that describes toDate
+ */
 function getRelativeText(fromDate: Date, toDate: Date) {
   const formatted = format(toDate, RELATIVE_FORMAT);
   if (isSameDay(fromDate, toDate)) {
     return `Today, ${formatted}`;
   } else if (isSameDay(addDays(fromDate, 1), toDate)) {
     return `Tomorrow, ${formatted}`;
+  } else if (isSameWeek(fromDate, toDate)) {
+    return `This ${format(toDate, "EEEE")}, ${formatted}`;
   }
   return `Next ${format(toDate, "EEEE")}, ${formatted}`;
 }
@@ -50,6 +74,12 @@ function getRelativeText(fromDate: Date, toDate: Date) {
 /**
  * Returns the next pickup date if its a recycling day, and a string of words that describe the next
  * day. If today is a pickup day, return that instead.
+ *
+ * This function hardcodes:
+ * - trash pickups to be on Wednesdays
+ * - recycling pickups to be only every other Wednesday with 2022-08-03T00:00:00-0500 as a reference
+ *   recycling pickup day.
+ *
  * @param fromDate The date of which to find the next pickup
  * @returns A Pickup object
  */
--- tests/pickup.test.ts
+++ tests/pickup.test.ts
@@ -14,8 +14,15 @@ test("pickup is tomorrow and has recycling", () => {
   expect(pickup.relativeText).toBe("Tomorrow, August 3rd");
 });
 
-test("pickup is day after tomorrow and has recycling", () => {
-  const pickup = getNextPickup(new Date("2022-08-01T12:34:56-0500"));
+test("pickup is this coming Wednesday (in this week) and has recycling", () => {
+  const pickup = getNextPickup(new Date("2022-08-01T12:34:56-0500")); // beginning of sunday
+  expect(pickup.date).toEqual(new Date("2022-08-03T00:00:00-0500"));
+  expect(pickup.isRecycling).toBe(true);
+  expect(pickup.relativeText).toBe("This Wednesday, August 3rd");
+});
+
+test("pickup is next Wednesday (in the next week) and has recycling", () => {
+  const pickup = getNextPickup(new Date("2022-07-30T12:34:56-0500")); // end of saturday
   expect(pickup.date).toEqual(new Date("2022-08-03T00:00:00-0500"));
   expect(pickup.isRecycling).toBe(true);
   expect(pickup.relativeText).toBe("Next Wednesday, August 3rd");
@@ -35,9 +42,16 @@ test("pickup is tomorrow and doesn't have recycling", () => {
   expect(pickup.relativeText).toBe("Tomorrow, August 10th");
 });
 
-test("pickup is day after tomorrow and doesn't have recycling", () => {
-  const pickup = getNextPickup(new Date("2022-08-08T12:34:56-0500"));
+test("pickup is this coming Wednesday (in the next week) and doesn't have recycling", () => {
+  const pickup = getNextPickup(new Date("2022-08-06T23:59:59-0500")); // end of saturday
   expect(pickup.date).toEqual(new Date("2022-08-10T00:00:00-0500"));
   expect(pickup.isRecycling).toBe(false);
   expect(pickup.relativeText).toBe("Next Wednesday, August 10th");
+});
+
+test("pickup is next Wednesday (in this week) and doesn't have recycling", () => {
+  const pickup = getNextPickup(new Date("2022-08-08T12:34:56-0500")); // beginning of sunday
+  expect(pickup.date).toEqual(new Date("2022-08-10T00:00:00-0500"));
+  expect(pickup.isRecycling).toBe(false);
+  expect(pickup.relativeText).toBe("This Wednesday, August 10th");
 });
