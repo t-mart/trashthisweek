8530c7b30253c5a86d2d3385de8e04f3829f49b8
Tim Martin <tim@timmart.in>
2025-01-24 05:10:06 -0600

    typo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 // import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Plausible from "@/components/plausible";
+import Plausible from "@/components/Plausible";
 
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
