bdf8b2600e932fe6fea688000aa1e03cb7cac047
Tim Martin <tim@timmart.in>
2025-01-24 15:08:51 -0600

    Static bin images

--- src/components/BinImage.tsx
+++ src/components/BinImage.tsx
@@ -1,25 +1,20 @@
 import Image from "next/image";
+import TrashBin from "@/images/trash.jpg";
+import RecyclingBin from "@/images/recycling.jpg";
 
 export type BinType = "trash" | "recycling";
 
-const imageWidth = 320;
-const imageHeight = 425;
-
-const trashSrc = "/trash.jpg";
 const trashAlt = "Trash bin";
 
-const recyclingSrc = "/recycling.jpg";
 const recyclingAlt = "Recycling bin";
 
 export default function BinImage({ binType }: { binType: BinType }) {
-  const src = binType === "trash" ? trashSrc : recyclingSrc;
+  const src = binType === "trash" ? TrashBin : RecyclingBin;
   const alt = binType === "trash" ? trashAlt : recyclingAlt;
   return (
     <Image
       src={src}
       alt={alt}
-      width={imageWidth}
-      height={imageHeight}
       className="rounded-lg shadow-lg w-full"
       priority
     />
